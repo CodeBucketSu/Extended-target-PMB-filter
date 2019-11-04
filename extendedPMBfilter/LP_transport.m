f9d91f8e26993073fb33a27d6749ada58d67554a
yuhsuansia <51372349+yuhsuansia@users.noreply.github.com>
2019-11-03 22:29:55 -0500

    Update LP_transport.m

--- extendedPMBfilter/LP_transport.m
+++ extendedPMBfilter/LP_transport.m
@@ -11,7 +11,17 @@ Aeq = [Aeq1;Aeq2];
 
 % can be largely speed up by using the Gurobi solver
 options = optimoptions('linprog','Algorithm','dual-simplex','Display','off');
-[x,Cmin] = linprog(c,[],[],Aeq,beq,zeros(1,H*N),ones(1,H*N),options);
+[x,Cmin] = linprog(c,[],[],sparse(Aeq),beq,zeros(1,H*N),ones(1,H*N),options);
+
+% params.outputflag = 0;          % Silence gurobi
+% params.method     = 1;          % Use dual simplex method
+% model_gurobi.A = sparse(Aeq);
+% model_gurobi.obj = c;
+% model_gurobi.sense = '=';
+% model_gurobi.rhs = beq;
+% result = gurobi(model_gurobi, params);
+% x = result.x;
+% Cmin = result.objval;
 q = reshape(x,N,H)';
 
 end
